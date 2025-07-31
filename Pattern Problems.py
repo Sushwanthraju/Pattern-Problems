f9dd7f1b41dec1b1f171d658c4bd096d5795510e
sushwanth raju r <sushwanthrajur@gmail.com>
2025-07-31 12:02:10 +0530

    Update Pattern Problems.py

--- Pattern Problems.py
+++ Pattern Problems.py
@@ -114,6 +114,45 @@ for i in range(n):
     print()
 
 
+#Hallow down_right-angle triangle.
+
+n=4
+for i in range(n):
+    for j in range(n):
+        if i==n-1 or j==0 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+
+
+#Hallow reverse right-angle triangle.
+    
+n=4
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i+j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+
+#Hallow up_right-angle triangle.
+
+n=4
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n-1 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
 
 
     
